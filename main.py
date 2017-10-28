b98e82bc1ba5a575c5520a4c38a6a5cb3fa75c2c
Kornel Rodziewicz <kornel.rodziewicz@itmagination.com>
2017-10-28 14:40:53 +0200

    fgsg

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("kornel")
+import requests
+# try: 
+from BeautifulSoup import BeautifulSoup
+# except ImportError:
+#     from bs4 import BeautifulSoup
+# b = requests.get('https://www.google.com')
+# urllib.request.urlopen
+
+print("a")
