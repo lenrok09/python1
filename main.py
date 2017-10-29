5d367ab61508181f90930ab240770605444d1731
Kornel Rodziewicz <kornel.rodziewicz@itmagination.com>
2017-10-29 10:52:43 +0100

    a

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import requests
+from bs4 import BeautifulSoup
 # try: 
-from BeautifulSoup import BeautifulSoup
+# from BeautifulSoup4 import BeautifulSoup
 # except ImportError:
 #     from bs4 import BeautifulSoup
-# b = requests.get('https://www.google.com')
+b = requests.get('https://ark.intel.com/pl/products/88193/Intel-Core-i5-6200U-Processor-3M-Cache-up-to-2_80-GHz')
 # urllib.request.urlopen
+parsed = BeautifulSoup(b.text, "html5lib")
+parsed.find_all("ul", attrs={"": "view-modal info-modal","data-modal": "tt-CoreCount"})
+# parsed.
 
-print("a")
+#tab-blade-1-0-1 > div > ul > li.CoreCount > span.label > a
+print(b.text)
