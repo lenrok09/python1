d4605cb4bcd077cb60916b16a173de4770d2a2fa
Kornel Rodziewicz <kornel.rodziewicz@itmagination.com>
2017-10-29 17:47:03 +0100

    mess with soup

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
-# try: 
-# from BeautifulSoup4 import BeautifulSoup
-# except ImportError:
-#     from bs4 import BeautifulSoup
-b = requests.get('https://ark.intel.com/pl/products/88193/Intel-Core-i5-6200U-Processor-3M-Cache-up-to-2_80-GHz')
-# urllib.request.urlopen
-parsed = BeautifulSoup(b.text, "html5lib")
-parsed.find_all("ul", attrs={"": "view-modal info-modal","data-modal": "tt-CoreCount"})
-# parsed.
 
+b = requests.get('https://ark.intel.com/pl/search?q=i5-6200U')
+
+parsed = BeautifulSoup(b.text, "html5lib")
+
+search_resut = parsed.find_all("h4",attrs={"class" : "result-title"})[0]
+
+try:
+    part_url = search_resut.contents[0]['href']
+    c = requests.get('https://ark.intel.com' + part_url)
+except:
+    print('fe')
+    
+
+parser_res = BeautifulSoup(c.text, "html5lib")
+search_specs = parser_res.find_all("ul",attrs={"class" : "specs-list"}) 
+specs = search_specs[1].find("li",attrs={"class" : "CoreCount"}).text    
 #tab-blade-1-0-1 > div > ul > li.CoreCount > span.label > a
 print(b.text)
